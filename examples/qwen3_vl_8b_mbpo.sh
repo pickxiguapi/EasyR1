23185efa1a98364c433a5e811f50b5439c2704c6
iffyuan <iffyuan@tencent.com>
2026-01-17 23:55:33 +0800

    Update

--- examples/qwen3_vl_8b_mbpo.sh
+++ examples/qwen3_vl_8b_mbpo.sh
@@ -3,6 +3,11 @@
 set -x
 
 MODEL_PATH=Qwen/Qwen3-VL-8B-Instruct
+ROLLOUT_BS=512
+GEN_BS=128
+GLOBAL_BS=128
+MB_PER_UPDATE=4
+MB_PER_EXP=8
 
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
@@ -13,8 +18,16 @@ python3 -m verl.trainer.main \
     worker.actor.clip_ratio_high=0.28 \
     worker.rollout.n=4 \
     worker.actor.optim.lr=1.0e-6 \
-    algorithm.disable_kl=True \
+    algorithm.disable_kl=False \
+    algorithm.use_kl_loss=True \
     algorithm.online_filtering=True \
     algorithm.adv_estimator=mbpo \
     trainer.experiment_name=qwen3_vl_8b_geo_mbpo \
-    trainer.n_gpus_per_node=8
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=8 \
+    data.rollout_batch_size="${ROLLOUT_BS}" \
+    data.mini_rollout_batch_size="${GEN_BS}" \
+    worker.actor.global_batch_size="${GLOBAL_BS}" \
+    worker.actor.micro_batch_size_per_device_for_update="${MB_PER_UPDATE}" \
+    worker.actor.micro_batch_size_per_device_for_experience="${MB_PER_EXP}" \
+    worker.rollout.tensor_parallel_size=2
