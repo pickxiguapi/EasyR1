ffa811d80057680b23acd5b15552bcf186b5a9a2
iffyuan <iffyuan@tencent.com>
2026-02-11 11:29:50 +0800

    Updtae

--- ER1.5_scripts/20260211_Embodied-R1.5_2node.sh
+++ ER1.5_scripts/20260211_Embodied-R1.5_2node.sh
@@ -4,16 +4,16 @@ set -x
 TRAIN_FILES="[rft_train_datasets/ER1.5_CoSyn-point_image_point.json,rft_train_datasets/ER1.5_Droid-Trace_image_trace.json,rft_train_datasets/ER1.5_EO_image_qa.json,rft_train_datasets/ER1.5_ER1-point_image_point.json,rft_train_datasets/ER1.5_ER1-trace_image_trace.json,rft_train_datasets/ER1.5_ERQA2_image_qa.json,rft_train_datasets/ER1.5_ERQA_Rush_image_qa.json,rft_train_datasets/ER1.5_general_image_qa_filtered.json,rft_train_datasets/ER1.5_HandAL_image_point.json,rft_train_datasets/ER1.5_HOI4D-Trace_image_trace.json,rft_train_datasets/ER1.5_InstructPart_image_point.json,rft_train_datasets/ER1.5_InternData-Trace_image_trace.json,rft_train_datasets/ER1.5_Ref_L4_image_point.json,rft_train_datasets/ER1.5_Refspatial_image_point.json,rft_train_datasets/ER1.5_regular_simulation_image_point.json,rft_train_datasets/ER1.5_regular_synthetic_image_point.json,rft_train_datasets/ER1.5_Robo2VLM_image_qa.json,rft_train_datasets/ER1.5_robocasa_partnet_2d_image_trace.json,rft_train_datasets/ER1.5_robocasa_partnet_3d_image_trace.json,rft_train_datasets/ER1.5_Roborefit_image_point.json,rft_train_datasets/ER1.5_RoboVQA_image.json,rft_train_datasets/ER1.5_SAT_image_qa.json,rft_train_datasets/ER1.5_spatialssrl_image_qa.json,rft_train_datasets/ER1.5_Temporal_image_qa.json]"
 TEST_FILES="[rft_test_datasets/erqa.json,rft_test_datasets/refspatial.json,rft_test_datasets/sat.json,rft_test_datasets/vabench_p.json,rft_test_datasets/where2place.json]"
 
-CONFIG=ER1.5_scripts/20260210_test.yaml
+CONFIG=/ER1.5_scripts/20260211_Embodied-R1.5_MBPO.yaml
 MODEL_PATH=/apdcephfs_wza/cientgu/iffyuan/models/Embodied-R1.5-SFT-0128
 IMAGE_DIR=/apdcephfs_wza/cientgu/iffyuan/Embodied-R1.5-RFT/data
-ROLLOUT_BS=512
-GEN_BS=128
-GLOBAL_BS=256
+ROLLOUT_BS=1024
+GEN_BS=256
+GLOBAL_BS=512
 MB_PER_UPDATE=4
 MB_PER_EXP=8
 REWARD=ER1.5_scripts/reward_function/embodied_reward.py:compute_score
-EXP_NAME=Embodied-R1.5-RFT-Test
+EXP_NAME=20260211_Embodied-R1.5_MBPO_only_image
 
 python3 -m verl.trainer.main \
     config=${CONFIG} \
@@ -24,10 +24,10 @@ python3 -m verl.trainer.main \
     algorithm.adv_estimator=mbpo \
     trainer.experiment_name=${EXP_NAME} \
     trainer.n_gpus_per_node=8 \
-    trainer.nnodes=1 \
-    trainer.val_freq=10 \
-    trainer.save_freq=10 \
-    trainer.save_limit=1 \
+    trainer.nnodes=2 \
+    trainer.val_freq=20 \
+    trainer.save_freq=15 \
+    trainer.save_limit=2 \
     data.rollout_batch_size="${ROLLOUT_BS}" \
     data.mini_rollout_batch_size="${GEN_BS}" \
     data.train_files=$TRAIN_FILES \
